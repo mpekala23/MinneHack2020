02d5e509b743522ae759bbed7ab798a36cdba849
mpekala23 <mpekala@college.harvard.edu>
2020-01-25 19:15:34 -0500

    Fixes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <flash></flash>
+    <div class="content-wrapper">
+      <navbar></navbar>
+      <router-view/>
+    </div>
+    <footbar></footbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FlashNotification from '@/common/Flash';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    "flash": FlashNotification,
   }
-}
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
